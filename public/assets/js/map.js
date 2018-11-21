a498aba1817c4894b535e261cb76483b42591d4d
Raka Suryaardi Widjaja <raka.suryadi@gmail.com>
2018-11-21 16:34:37 +0700

    [ADD] map detail

--- public/assets/js/map.js
+++ public/assets/js/map.js
@@ -1,3 +1,7 @@
+const photo = document.getElementById('map-photo');
+const title = document.getElementById('map-title');
+const description = document.getElementById('map-description');
+
 var map = L.map('leaflet-map').setView([-6.932306, 107.594005], 14);
 map.scrollWheelZoom.disable();
 
@@ -14,17 +18,24 @@ map.on('click', function(e) {
 });
 
 var places = [
-  {location: [-6.9484956, 107.5901394], name: "CGV Miko Mall", description: "Miko Mall, Jln Kopo Cirangrang No.599, Cirangrang, Babakan Ciparay, Kota Bandung, Jawa Barat 40227", image: "mikomall.jpg"},
-  {location: [-6.9437243,107.5930576], name: "Festival Citylink XXI", description: "Lt.3A, Jl. Peta No.241, Suka Asih, Bojongloa Kaler, Kota Bandung, Jawa Barat 40232", image: "citylink.jpg"},
-  {location: [-6.9439185,107.5953811], name: "XXI Transmart Buahbatu Square", description: "Buahbatu Square Lantai 1, Jalan Raya No. 259, Cipagalo, Bojongsoang, Cipagalo, Bojongsoang, Bandung, Jawa Barat 40287", image: "transmart.jpg"},
-  {location: [-6.9128304,107.5918609], name: "CGV 23 Paskal", description: "23 Paskal Shopping Mall, Jl. Pasir Kaliki, Kb. Jeruk, Andir, Kota Bandung, Jawa Barat 40182", image: "paskal.jpg"},
-  {location: [-6.9236753,107.5917396], name: "Braga XXI", description: "Braga City Walk Lantai 2, Jl. Braga 99-101, Braga, Sumur Bandung, Braga, Sumur Bandung, Kota Bandung, Jawa Barat 40111", image: "braga.jpg"}
+  {location: [-6.9484956, 107.5901394], name: "CGV Miko Mall", description: "Miko Mall, Jln Kopo Cirangrang No.599, Cirangrang, Babakan Ciparay, Kota Bandung, Jawa Barat 40227", photo: "mikomall.jpg"},
+  {location: [-6.9437243,107.5930576], name: "Festival Citylink XXI", description: "Lt.3A, Jl. Peta No.241, Suka Asih, Bojongloa Kaler, Kota Bandung, Jawa Barat 40232", photo: "citylink.jpg"},
+  {location: [-6.9439185,107.5953811], name: "XXI Transmart Buahbatu Square", description: "Buahbatu Square Lantai 1, Jalan Raya No. 259, Cipagalo, Bojongsoang, Cipagalo, Bojongsoang, Bandung, Jawa Barat 40287", photo: "transmart.jpg"},
+  {location: [-6.9128304,107.5918609], name: "CGV 23 Paskal", description: "23 Paskal Shopping Mall, Jl. Pasir Kaliki, Kb. Jeruk, Andir, Kota Bandung, Jawa Barat 40182", photo: "paskal.jpg"},
+  {location: [-6.9236753,107.5917396], name: "Braga XXI", description: "Braga City Walk Lantai 2, Jl. Braga 99-101, Braga, Sumur Bandung, Braga, Sumur Bandung, Kota Bandung, Jawa Barat 40111", photo: "braga.jpg"}
 ];
 
 var poly = [];
 for (var place of places) {
-  L.marker(place.location).addTo(map).bindPopup(place.name);
-  poly.push(place.location);  
+  L.marker(place.location).addTo(map).bindPopup(place.name).on('click', L.bind(showDetail, null, place));
+  poly.push(place.location);
 }
 
+function showDetail(place) {
+  title.innerHTML = place.name;
+  description.innerHTML = place.description;
+  photo.setAttribute('src', 'assets/image/' + place.photo);
+  map.setView([place.location[0], place.location[1]], 13);
+}
+showDetail(places[places.length - 1]);
 L.polygon(poly).addTo(map).bindPopup("Ruang Hampa >_<");
